1a27a5c40e3f9b8d5f358d2153bfdc3763655831
William <William.Jeffery.Knight@protonmail.com>
2022-08-28 17:10:55 -0400

    use both state in app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function App() {
-  // dont use a state object
-  // const [state, setState] = ({count: 4, theme: 'blue'})
-  // instead use to different states values
-  const [count, setCount] = useState(4)
-  const [theme, setTheme] = useState(4)
-  const decrementCount = () =>{
-    setCount((prevCount) => prevCount - 1)
-  };
-  const increaseCount = () => {
-    setCount((prevCount) => prevCount + 1)
-  };
-  
-  return (
-    <>
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={increaseCount}>+</button>
-    </>
-  )
+    // dont use a state object
+    // const [state, setState] = ({count: 4, theme: 'blue'})
+    // instead use two different states values
+    const [theme, setTheme] = useState("Blue");
+    const [count, setCount] = useState(4);
+    const decrementCount = () => {
+        setCount((prevCount) => prevCount - 1);
+    };
+    const increaseCount = () => {
+        setCount((prevCount) => prevCount + 1);
+    };
 
-
+    return (
+        <>
+            <button onClick={decrementCount}>-</button>
+            <span>{count}</span>
+            <button onClick={increaseCount}>+</button>
+            <p><span>{theme}</span></p>
+        </>
+    );
 }
 
 export default App;
